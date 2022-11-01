7b32df7aa76f12a221695e5968ac92d19cb9e461
Kleyser Mejia <kleyser57@gmail.com>
2022-10-31 21:16:47 -0600

    Reloj

---
+++ index.js
@@ -0,0 +1,55 @@
+const $days = document.getElementById('days'),
+    $hours = document.getElementById('hours'),
+    $minutes = document.getElementById('minutes'),
+    $seconds = document.getElementById('seconds');
+// $finalMessage = document.querySelector('.final-sms');
+
+
+
+
+var div = document.querySelector('.deepshadow');
+var b = document.getElementById('n');
+var p = document.querySelector('.legend');
+b.disabled = true;
+
+
+
+
+//Fecha a futuro
+const countdownDate = new Date('11 01, 2022 8:00 AM').getTime();
+
+let interval = setInterval(function () {
+    //Obtener fecha actual y milisegundos
+    const now = new Date().getTime();
+
+    //Obtener las distancias entre ambas fechas
+    let distance = countdownDate - now;
+
+    //Calculos a dias-horas-minutos-segundos
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / (1000));
+
+    //Escribimos resultados
+    // $days.innerHTML = days;
+    $hours.innerHTML = hours;
+    $minutes.innerHTML = minutes;
+    $seconds.innerHTML = ('0' + seconds).slice(-2);
+
+    //Cuando llegue a 0
+    if (distance < 0) {
+        clearInterval(interval);
+        // $days.innerHTML = 0;
+        $hours.innerHTML = 0;
+        $minutes.innerHTML = 0;
+        $seconds.innerHTML = 0;
+        // p.innerHTML = `<p class="legend">Las prematriculas cerraran en: </p>`
+        // $finalMessage.style.transform = 'translateY(0)';
+        div.innerHTML = `<h2 class='deepshadow'>Prematriculas abiertas.</h2>`;
+        b.disabled = false;
+        document.querySelector('.pie').innerHTML = "<div></div>"
+        document.querySelector('.contenedor-d').classList.add('aux');
+        document.querySelector('.aux').classList.remove('contenedor-d');
+    }
+}, 1000);
